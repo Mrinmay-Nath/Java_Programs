8735187de50b39506d66788b5231e120bbd28e04
Mrinmay Nath <87058051+Mrinmay-Nath@users.noreply.github.com>
2023-05-02 21:29:48 +0530

    Add files via upload
    /*write a program to calculate the area and perimeter of a rectangle where value is already
    present in the program*/

---
+++ rectangle.java
@@ -0,0 +1,12 @@
+/*write a program to calculate the area and perimeter of a rectangle where value is already
+present in the program*/
+class rectangle 
+{
+    public static void main(String x[])
+    {
+        int length=20,bredth=30;
+        double perimeter=2*(length+bredth);
+        double area=length*bredth;
+        System.out.println("Area is "+area+" perimeter is "+perimeter);
+    }  
+}
