0b0c0579cf8b59f920523bd625f49dc7d8421f81
Mrinmay Nath <87058051+Mrinmay-Nath@users.noreply.github.com>
2023-05-02 21:25:19 +0530

    Add files via upload
    /*write a program to calculate the area and perimeter of a square where value is already
    present in the program*/

---
+++ Square.java
@@ -0,0 +1,12 @@
+/*write a program to calculate the area and perimeter of a square where value is already
+present in the program*/
+class Square 
+{
+    public static void main(String x[])
+    {
+        double length=30;
+        double area=(double)length*length;
+        double perimeter=(double)4*length;
+        System.out.println("Area is "+area+"Perimeter is perimeter "+perimeter);
+    }   
+}
