45a0dfd22a3c8feeaeaab951ee1429c4405c788f
Mrinmay Nath <87058051+Mrinmay-Nath@users.noreply.github.com>
2023-05-03 17:28:13 +0530

    Add files via upload
    print Reversed Pyramid pattern

---
+++ pattern3.java
@@ -0,0 +1,20 @@
+/* print Reversed Pyramid pattern 
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+*/
+public class pattern3 {
+    public static void main(String[] args) {
+        int i, j;
+        for (i = 0; i < 5; i++) {
+            for (j = i; j <5; j++) {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+
+    }
+
+}
